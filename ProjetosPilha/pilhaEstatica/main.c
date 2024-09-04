ac55735a21f502d96c423e8bc200d36a83c74a5f
Rodrigo Almeida Dourado <115838474+Rodrigo-Dourad0@users.noreply.github.com>
2024-09-04 00:14:44 -0300

    mudei tudo

--- ProjetosPilha/pilhaEstatica/main.c
+++ ProjetosPilha/pilhaEstatica/main.c
@@ -1,26 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "pilha.h"
-
 
 int main(){
 
-  Pilha p ;
-  incializarPilha(&p);
-
-  empilhar(&p, 5);
-  empilhar(&p, 10);
-  empilhar(&p, 15);
-
-  exibirPilha(&p);
-
-  printf("\n\n");
-  
- int desempilhado = desempilhar(&p);
-
-  exibirPilha(&p);
-
- printf("Este item foi desempilhado: %d", desempilhado);
-
-  return 0;
+    printf("Ola mundo");
+    return 0;
+    
 }
